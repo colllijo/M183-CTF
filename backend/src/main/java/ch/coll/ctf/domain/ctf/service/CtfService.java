5637081b811cb030b56af009c10d79db3e74a515
laptopAdmin <arunannagarajah@bbzbl-it.ch>
2024-05-28 11:15:47 +0200

    added CTF service in and out ports

---
+++ backend/src/main/java/ch/coll/ctf/domain/ctf/service/CtfService.java
@@ -0,0 +1,42 @@
+package ch.coll.ctf.domain.ctf.service;
+
+import ch.coll.ctf.domain.ctf.model.CaptureTheFlag;
+import ch.coll.ctf.domain.ctf.port.in.CtfServicePort;
+import ch.coll.ctf.domain.ctf.port.out.CtfRepositoryPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CtfService implements CtfServicePort {
+
+    private final CtfRepositoryPort ctfRepositoryPort;
+
+    @Override
+    public List<CaptureTheFlag> getAllChallenges() {
+        return ctfRepositoryPort.findAll();
+    }
+
+    @Override
+    public CaptureTheFlag getChallengeById(Long id) {
+        return ctfRepositoryPort.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Challenge with id " + id + " not found"));
+    }
+
+    @Override
+    public CaptureTheFlag createChallenge(CaptureTheFlag challenge) {
+        return ctfRepositoryPort.save(challenge);
+    }
+
+    @Override
+    public CaptureTheFlag updateChallenge(Long id, CaptureTheFlag ctf) {
+        return ctfRepositoryPort.save(ctf);
+    }
+
+    @Override
+    public void deleteChallenge(Long id) {
+        ctfRepositoryPort.deleteById(id);
+    }
+}
