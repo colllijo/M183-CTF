e3b0e2752cd6e6546544fb544c28763b85ece02c
Liam Metzger <liam.metzger@blkb.ch>
2024-06-24 20:29:28 +0200

    Fix mapping

--- backend/src/main/java/ch/coll/ctf/adapter/repository/jpa/ctf/mapper/CtfEntityMapper.java
+++ backend/src/main/java/ch/coll/ctf/adapter/repository/jpa/ctf/mapper/CtfEntityMapper.java
@@ -1,19 +1,28 @@
 package ch.coll.ctf.adapter.repository.jpa.ctf.mapper;
 
-import ch.coll.ctf.adapter.repository.jpa.ctf.entity.CtfEntity;
-import ch.coll.ctf.domain.ctf.model.Ctf;
+import org.mapstruct.Context;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 import ch.coll.ctf.adapter.repository.jpa.EntityMapperConfig;
+import ch.coll.ctf.adapter.repository.jpa.ctf.entity.CtfEntity;
 import ch.coll.ctf.adapter.repository.jpa.user.mapper.UserEntityMapper;
+import ch.coll.ctf.domain.ctf.model.Ctf;
+import ch.coll.ctf.domain.mapping.model.CycleAvoidingMappingContext;
+import ch.coll.ctf.domain.mapping.model.DoIgnore;
 
 @Mapper(config = EntityMapperConfig.class, uses = {SolveEntityMapper.class, UserEntityMapper.class})
 public interface CtfEntityMapper {
-    public Ctf mapEntityToModel(CtfEntity entity);
+  @DoIgnore
+  public default Ctf mapEntityToModel(CtfEntity entity) {
+    return mapEntityToModel(entity, new CycleAvoidingMappingContext());
+  }
 
-    @Mapping(target = "id", ignore = true)
-    public CtfEntity mapModelToEntity(Ctf model);
+  public Ctf mapEntityToModel(CtfEntity entity, @Context CycleAvoidingMappingContext context);
 
+  @DoIgnore
+  public default CtfEntity mapModelToEntity(Ctf model) {
+    return mapModelToEntity(model, new CycleAvoidingMappingContext());
+  }
 
+  public CtfEntity mapModelToEntity(Ctf model, @Context CycleAvoidingMappingContext context);
 }
